75002c4ed7459db84fb156947680821dcac5f2fd
Marcos Oliveira <marcos@evolve.tec.br>
2023-03-07 09:21:22 -0300

    feat: hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
   return (
     <section className="relative w-full h-screen mx-auto">
       <div
-        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
+        className={`absolute inset-0 top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915eff]" />
@@ -18,9 +18,9 @@ const Hero = () => {
             Hi, I'm <span className="text-[#915eff]">Marcos</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I developer Web, studies for 3D visuals, user
+            I developer user interfaces
             <br className="sm:block hidden" />
-            interfaces and web applications
+            and web applications
           </p>
         </div>
       </div>
@@ -30,7 +30,7 @@ const Hero = () => {
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
-            <motion.dev
+            <motion.div
               animate={{
                 y: [0, 24, 0],
               }}
